b11b7839bb1242bbdbce4f28365ac1b910505781
Qijie Zhu <qzhu9@ucsc.edu>
2021-10-07 21:42:02 -0700

    fixed up UI

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ import tkinter
 
 LABEL_FONT = ("Arial", 12)
 BUTTON_FONT = ("Arial", 10)
+ENTRY_FONT = ("Arial", 10)
 
 # Create and open GUI window
 app_window = tkinter.Tk()
 app_window.title("Password Manager")
-app_window.minsize(width=500, height=350)
+app_window.minsize(width=550, height=400)
 app_window.config(padx=20, pady=20)
 
 # Logo
 app_logo = tkinter.PhotoImage(file="logo.png")
-logo_canvas = tkinter.Canvas(width=200, height=200, highlightthickness=0)
-logo_canvas.create_image(100, 100, image=app_logo)
+logo_canvas = tkinter.Canvas(width=250, height=200, highlightthickness=0)
+logo_canvas.create_image(130, 100, image=app_logo)
 logo_canvas.grid(row=1, column=2)
 
 # Labels
@@ -29,17 +30,17 @@ password_label = tkinter.Label(text="Password: ", font=LABEL_FONT)
 password_label.grid(row=4, column=1)
 
 # Website Entry
-website_entry = tkinter.Entry(width=36)
+website_entry = tkinter.Entry(width=42, font=ENTRY_FONT)
 website_entry.focus()
-website_entry.grid(row=2, column=2, columnspan=2, sticky="W")
+website_entry.grid(row=2, column=2, columnspan=2, sticky="W", pady=2)
 
 # Username/Email Entry
-username_entry = tkinter.Entry(width=36)
-username_entry.grid(row=3, column=2, columnspan=2, sticky="W")
+username_entry = tkinter.Entry(width=42, font=ENTRY_FONT)
+username_entry.grid(row=3, column=2, columnspan=2, sticky="W", pady=2)
 
 # Password Entry
-password_entry = tkinter.Entry(width=28)
-password_entry.grid(row=4, column=2, sticky="W")
+password_entry = tkinter.Entry(width=28, font=ENTRY_FONT)
+password_entry.grid(row=4, column=2, sticky="W", pady=2)
 
 # "Generate Password" button
 generatepassword_button = tkinter.Button(text="Generate Password", font=BUTTON_FONT)
